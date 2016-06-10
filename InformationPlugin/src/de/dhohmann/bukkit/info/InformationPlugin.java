5e9521f27a321f0909413afac04fc15a883e400e
dhohmann <dtwin95@gmx.de>
2016-06-10 15:16:36 +0200

    Added reload logic

--- InformationPlugin/src/de/dhohmann/bukkit/info/InformationPlugin.java
+++ InformationPlugin/src/de/dhohmann/bukkit/info/InformationPlugin.java
@@ -2,7 +2,6 @@ package de.dhohmann.bukkit.info;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -26,7 +25,17 @@ public class InformationPlugin extends JavaPlugin {
 	 */
 	public void onEnable() {
 		loadConfig();
-		players = new ArrayList<>();
+		if (players == null) {
+			players = new ArrayList<>();
+		}
+		task = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+
+			@Override
+			public void run() {
+				updateScoreboard();
+			}
+
+		}, 0, 20);
 	}
 
 	/**
@@ -36,15 +45,6 @@ public class InformationPlugin extends JavaPlugin {
 		saveDefaultConfig();
 		getConfig().options().copyDefaults();
 		saveConfig();
-
-		task = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-
-			@Override
-			public void run() {
-				updateScoreboard();
-			}
-
-		}, 0, 20);
 	}
 
 	/**
@@ -76,8 +76,9 @@ public class InformationPlugin extends JavaPlugin {
 			tmp = tmp.replace("%worldtime%", timeToString(this.getServer().getWorlds().get(0).getTime()));
 			tmp = tmp.replace("%flight_allowed%", Boolean.toString(this.getServer().getAllowFlight()));
 			tmp = tmp.replace("%online_players%", Integer.toString(this.getServer().getOnlinePlayers().size()));
-			tmp = tmp.replace("%plugin_list%", Arrays.deepToString(this.getServer().getPluginManager().getPlugins()));
 			tmp = tmp.replace("%difficulty%", this.getServer().getWorlds().get(0).getDifficulty().toString());
+			tmp = tmp.replace("%server%",
+					(getServer().getIp().equals("") ? "localhost" : getServer().getIp()) + ":" + getServer().getPort());
 			scores.add(obj.getScore(tmp));
 		}
 
@@ -92,10 +93,14 @@ public class InformationPlugin extends JavaPlugin {
 			if (!p.isOnline()) {
 				continue;
 			}
-			if (!players.contains(p)) {
-				p.setScoreboard(Bukkit.getScoreboardManager().getMainScoreboard());
-			} else {
-				p.setScoreboard(board);
+			try {
+				if (!players.contains(p)) {
+					p.setScoreboard(Bukkit.getScoreboardManager().getMainScoreboard());
+				} else {
+					p.setScoreboard(board);
+				}
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
 			}
 		}
 	}
@@ -111,13 +116,19 @@ public class InformationPlugin extends JavaPlugin {
 	 * Triggered when the player adds the defined command
 	 */
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (command.getName().equals("information")) {
+		if (label.equals("itoggle") || command.getName().equalsIgnoreCase("info-toggle")) {
 			Player p = (Player) sender;
 			if (players.contains(p)) {
 				players.remove(p);
 			} else {
 				players.add(p);
 			}
+			return true;
+		}
+		if (label.equals("ireload") || command.getName().equalsIgnoreCase("info-reload")) {
+			System.out.println("[INFO] Reloading config for InformationPlugin");
+			reloadConfig();
+			return true;
 		}
 		return false;
 	}
